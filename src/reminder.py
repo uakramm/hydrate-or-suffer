5c228e78bf93e1c0ce70c640ef026c82bcc49b11
Ushna <ushna.akram23@gmail.com>
2025-03-27 01:23:14 +0500

    twilio api call fix

--- src/reminder.py
+++ src/reminder.py
@@ -4,7 +4,7 @@ from utils import get_random_insult
 
 def send_whatsapp_message(body):
     client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
-    message = client.messages.create(
+    message = client.api.account.messages.create(
         from_=TWILIO_WHATSAPP_FROM,
         body=body,
         to=WHATSAPP_TO
